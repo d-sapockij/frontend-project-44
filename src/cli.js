bc95fab916e754f5f8598a51fca24494264ed10e
Daniil Sapotsky <asapotsk@bk.ru>
2024-04-01 20:00:34 +0700

    4 step

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,12 @@
 import readlineSync from 'readline-sync';
 
 const getName = () => {
-    const name = readlineSync.question('May I have your name? ');
-    return name;
+  const name = readlineSync.question('May I have your name? ');
+  return name;
 };
 
 const greet = () => {
-    console.log(`Hello, ${getName()}`);
+  console.log(`Hello, ${getName()}`);
 };
 
-export { greet };
+export default greet;
